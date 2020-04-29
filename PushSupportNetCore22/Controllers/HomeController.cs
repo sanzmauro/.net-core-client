e57c6836ac602b73ab1a125de66d2ddedfbb2588
sanzmauro <mauro.sanz@split.io>
2020-04-29 15:36:46 -0300

    asp net core 2.2

---
+++ PushSupportNetCore22/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PushSupportNetCore22.Models;
+using Splitio.Services.Client.Interfaces;
+using System.Diagnostics;
+
+namespace PushSupportNetCore22.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISplitClient _sdk;
+
+        public HomeController(MyAppData data)
+        {
+            _sdk = data.Sdk;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Treatment = _sdk.GetTreatment("admin", "mauro_net");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PushSupportNetCore22/MyAppData.cs
@@ -0,0 +1,34 @@
+﻿using Splitio.Services.Client.Classes;
+using Splitio.Services.Client.Interfaces;
+
+namespace PushSupportNetCore22
+{
+    public class MyAppData
+    {
+        public ISplitClient Sdk { get; set; }
+        public ConfigurationOptions Config { get; set; }
+
+        public MyAppData()
+        {
+
+            var apikey = "gmadcr3k2qr7kfhu34lemso7v341hfipmiha";
+
+            var config = new ConfigurationOptions
+            {
+                Endpoint = "https://sdk.split-stage.io",
+                EventsEndpoint = "https://events.split-stage.io",
+                StreamingEnabled = true,
+                FeaturesRefreshRate = 50000,
+                SegmentsRefreshRate = 50000
+            };
+
+            var factory = new SplitFactory(apikey, config);
+            var sdk = factory.Client();
+
+            Sdk = sdk;
+            Config = config;
+
+            Sdk.BlockUntilReady(10000);
+        }
+    }
+}
